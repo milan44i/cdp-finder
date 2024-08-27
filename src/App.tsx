81f1470bc81c7fa9e094c7be4bd49084c91bad17
Milan Stankovic <mln6stankovic@gmail.com>
2024-08-27 11:00:18 +0200

    dynamic import of components in App.tsx to reduce chuck sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { ReactElement } from 'react'
+import { ReactElement, Suspense, lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { MetaMaskInpageProvider } from '@metamask/providers'
-
-import CdpFinder from './pages/CdpFinder'
-import Header from './components/Header'
-import CdpPage from './pages/CdpPage/CdpPage'
-import ErrorPage from './pages/ErrorPage'
 
 declare global {
   interface Window {
@@ -13,15 +8,22 @@ declare global {
   }
 }
 
+const CdpFinder = lazy(() => import('./pages/CdpFinder'))
+const CdpPage = lazy(() => import('./pages/CdpPage/CdpPage'))
+const ErrorPage = lazy(() => import('./pages/ErrorPage'))
+const Header = lazy(() => import('./components/Header'))
+
 export default function Home(): ReactElement {
   return (
     <div className="flex flex-col gap-10 items-center pt-10 pb-10 h-full bg-gradient-to-r from-[#0093E9] to-[#80D0C7] text-white">
-      <Header />
-      <Routes>
-        <Route path="/" element={<CdpFinder />} />
-        <Route path="/cdp/:cdpId" element={<CdpPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<CdpFinder />} />
+          <Route path="/cdp/:cdpId" element={<CdpPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
